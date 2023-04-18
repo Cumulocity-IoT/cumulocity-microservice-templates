9d628978a448031915fcd925cea8588073c4f8f7
Pester <apes@softwareag.com>
2023-04-18 13:26:47 +0200

    code improvements

--- ipc/src/main/java/com/c8y/ms/templates/ipc/service/TemplatesBasicClientConfig.java
+++ ipc/src/main/java/com/c8y/ms/templates/ipc/service/TemplatesBasicClientConfig.java
@@ -17,17 +17,21 @@ import feign.RequestInterceptor;
 
 @Configuration
 public class TemplatesBasicClientConfig {
-	
+
 	private final Logger log = LoggerFactory.getLogger(TemplatesBasicClient.class);
 
 	private final ContextService<UserCredentials> userContextService;
-	
+
 	private final ContextService<MicroserviceCredentials> microserviceContextService;
+
+	private final CumulocityClientProperties clientProperties;
 	
-    private final CumulocityClientProperties clientProperties;
+	private final String microserviceName = "templates-basic";
 
 	@Autowired
-	public TemplatesBasicClientConfig(ContextService<UserCredentials> userContextService, ContextService<MicroserviceCredentials> microserviceContextService, CumulocityClientProperties clientProperties) {
+	public TemplatesBasicClientConfig(ContextService<UserCredentials> userContextService,
+			ContextService<MicroserviceCredentials> microserviceContextService,
+			CumulocityClientProperties clientProperties) {
 		this.userContextService = userContextService;
 		this.microserviceContextService = microserviceContextService;
 		this.clientProperties = clientProperties;
@@ -36,28 +40,28 @@ public class TemplatesBasicClientConfig {
 	@Bean
 	public RequestInterceptor requestInterceptor() {
 		return requestTemplate -> {
-			
+
 			String tenant;
 			String username;
 			String password;
-			
-			//check if user context available
-			if(userContextService.isInContext()) {
+
+			// Use user credentials if available otherwise use microservice credentials
+			if (userContextService.isInContext()) {
 				log.info("User context!!!");
 				tenant = userContextService.getContext().getTenant();
 				username = userContextService.getContext().getUsername();
 				password = userContextService.getContext().getPassword();
-			}else {
+			} else {
 				log.info("Microservice context!!!");
 				tenant = microserviceContextService.getContext().getTenant();
 				username = microserviceContextService.getContext().getUsername();
 				password = microserviceContextService.getContext().getPassword();
 			}
-			
+
 			String credentials = tenant + "/" + username + ":" + password;
 			String base64Credentials = Base64.getEncoder().encodeToString(credentials.getBytes());
 			requestTemplate.header("Authorization", "Basic " + base64Credentials);
-	    	String url = clientProperties.getBaseURL() + "/service/templates-basic";
+			String url = clientProperties.getBaseURL() + "/service/" + microserviceName;
 			requestTemplate.target(url);
 		};
 	}
