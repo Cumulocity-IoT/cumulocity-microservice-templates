c94a65d0845040efbfea2fe77c2ed3e9588104ad
Stefan Witschel <Stefan.Witschel@softwareag.com>
2024-06-28 14:27:39 +0200

    Adding more logging output

--- async/src/main/java/com/c8y/ms/templates/async/service/EventService.java
+++ async/src/main/java/com/c8y/ms/templates/async/service/EventService.java
@@ -11,14 +11,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.*;
 
 
 @Service
+@EnableAsync
 public class EventService {
 
     private static final Logger log = LoggerFactory.getLogger(EventService.class);
@@ -36,29 +39,40 @@ public class EventService {
     ExecutorService virtExecutorService = Executors.newVirtualThreadPerTaskExecutor();
 
 
-    public List<EventRepresentation> getEvents0() {
-        return eventApi.getEvents().get(10).getEvents();
+    public List<EventRepresentation> getEvents0()  {
+        log.info("Simulating load...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        List<EventRepresentation> eventList = eventApi.getEvents().get(10).getEvents();
+        log.info("Events found: {}", eventList);
+        log.info("Returning result!");
+        return eventList;
     }
 
     /**
      * Method using CompletableFuture to call eventAPI asynchronously
      **/
-    public CompletableFuture<List<EventRepresentation>> getEvents1()  {
+    public CompletableFuture<List<EventRepresentation>> getEvents1() {
         var eventFuture = new CompletableFuture();
-
         CompletableFuture.runAsync(() -> {
-            try {
                 subscriptionsService.runForEachTenant(() -> {
-                    log.info("Retrieving events...");
-                    eventFuture.complete(eventApi.getEvents().get(10).getEvents());
-                    log.info("Retrieving events completed!");
+                    try {
+                        log.info("Simulating load...");
+                        Thread.sleep(2000);
+                        List<EventRepresentation> eventList = eventApi.getEvents().get(10).getEvents();
+                        eventFuture.complete(eventApi.getEvents().get(10).getEvents());
+                        log.info("Events found: {}", eventList);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        CompletableFuture.failedFuture(e);
+                    }
                 });
-            } catch (SDKException e) {
-                e.printStackTrace();
-                CompletableFuture.failedFuture(e);
-            }
-        });
 
+        });
+        log.info("Returning result!");
         return eventFuture;
     }
 
@@ -68,19 +82,25 @@ public class EventService {
     @Async
     public CompletableFuture<List<EventRepresentation>> getEvents2()  {
         var eventFuture = new CompletableFuture();
-        try {
-            log.info("Retrieving events...");
-            eventFuture.complete(eventApi.getEvents().get(10).getEvents());
-            log.info("Retrieving events completed!");
-        } catch (SDKException e) {
-            eventFuture.completeExceptionally(e);
-        }
+        subscriptionsService.runForEachTenant(() -> {
+            try {
+                log.info("Simulating load...");
+                Thread.sleep(2000);
+                List<EventRepresentation> eventList = eventApi.getEvents().get(10).getEvents();
+                log.info("Events found: {}", eventList);
+                eventFuture.complete(eventList);
+            } catch (Exception e) {
+                eventFuture.completeExceptionally(e);
+            }
+        });
+        log.info("Returning result!");
         return eventFuture;
     }
     /**
      * Method using ExecutorService to create a new thread retrieving events
      **/
     public Future<List<EventRepresentation>> getEvents3() throws ExecutionException, InterruptedException {
+        log.info("Returning result!");
         return executorService.submit( new EventRetrievalTask<>());
     }
 
@@ -90,11 +110,16 @@ public class EventService {
         public List<EventRepresentation> call() throws Exception {
             List<EventRepresentation> eventList = new ArrayList<>();
             subscriptionsService.runForEachTenant(() -> {
-                log.info("Retrieving events...");
+                log.info("Simulating load...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 eventList.addAll(eventApi.getEvents().get(10).getEvents());
-                log.info("Retrieving events completed!");
-            });
+                log.info("Events found: {}", eventList);
 
+            });
             return eventList;
         }
     }
@@ -103,6 +128,10 @@ public class EventService {
      * Method using VirtualThreads + CompletableFuture to create a new thread retrieving events
      **/
     public Future<List<EventRepresentation>> getEvents4() throws InterruptedException {
-        return  virtExecutorService.submit(new EventRetrievalTask<>());
+        log.info("Returning result!");
+        return virtExecutorService.submit(new EventRetrievalTask<>());
     }
+
+
+
  }
