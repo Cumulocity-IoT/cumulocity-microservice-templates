033bcf0960eb38ddd79897cde5a54fcab42cfdd6
Pester <apes@softwareag.com>
2024-07-30 15:05:47 +0200

    add async example

---
+++ async/src/test/java/com/c8y/ms/templates/basic/AppTest.java
@@ -0,0 +1,13 @@
+package com.c8y.ms.templates.basic;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
